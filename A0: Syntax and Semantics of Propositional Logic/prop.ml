126bfc1c945679515960e8f52bce7ea0cf025c3e
yash98 <yashmalviya98@gmail.com>
2019-08-20 18:41:59 +0530

    complete A0

--- A0: Syntax and Semantics of Propositional Logic/prop.ml
+++ A0: Syntax and Semantics of Propositional Logic/prop.ml
@@ -46,15 +46,15 @@ let rec subprop_at_h (p: prop) (sp: prop) (csl: string) (sls: string list): stri
   | T -> sls
   | F -> sls
   | L(s) -> sls
-  | Not(p1) -> let csl1 = "B"^csl in subprop_at_h p1 sp csl1 sls
-  | And(p1, p2) -> let csl1 = "L"^csl in let f = subprop_at_h p1 sp csl1 sls in 
-    let csl2 = "R"^csl in subprop_at_h p1 sp csl2 f
-  | Or(p1, p2) -> let csl1 = "L"^csl in let f = subprop_at_h p1 sp csl1 sls in 
-    let csl2 = "R"^csl in subprop_at_h p1 sp csl2 f
-  | Impl(p1, p2) -> let csl1 = "L"^csl in let f = subprop_at_h p1 sp csl1 sls in 
-    let csl2 = "R"^csl in subprop_at_h p1 sp csl2 f
-  | Iff(p1, p2) -> let csl1 = "L"^csl in let f = subprop_at_h p1 sp csl1 sls in 
-    let csl2 = "R"^csl in subprop_at_h p1 sp csl2 f;;
+  | Not(p1) -> let csl1 = csl^"B" in subprop_at_h p1 sp csl1 sls
+  | And(p1, p2) -> let csl1 = csl^"L" in let f = subprop_at_h p1 sp csl1 sls in 
+    let csl2 = csl^"R" in subprop_at_h p2 sp csl2 f
+  | Or(p1, p2) -> let csl1 = csl^"L" in let f = subprop_at_h p1 sp csl1 sls in 
+    let csl2 = csl^"R" in subprop_at_h p2 sp csl2 f
+  | Impl(p1, p2) -> let csl1 = csl^"L" in let f = subprop_at_h p1 sp csl1 sls in 
+    let csl2 = csl^"R" in subprop_at_h p2 sp csl2 f
+  | Iff(p1, p2) -> let csl1 = csl^"L" in let f = subprop_at_h p1 sp csl1 sls in 
+    let csl2 = csl^"R" in subprop_at_h p2 sp csl2 f;;
 
 exception NotSubProp;;
 
@@ -69,7 +69,7 @@ exception NOT_IN_RHO;;
 let rec truth (p: prop) (r: rho):  bool = match p with
   | T -> true
   | F -> false
-  | Not(p1) -> not(truth p r)
+  | Not(p1) -> not(truth p1 r)
   | And(p1, p2) -> (truth p1 r) && (truth p2 r)
   | Or(p1, p2) -> (truth p1 r) || (truth p2 r)
   | Impl(p1, p2) -> let p3 = Or(Not(p1), p2) in truth p3 r
@@ -113,12 +113,132 @@ let rec dnf_i (p: prop): prop = match p with
   | Or(p1, p2) -> Or(dnf_i(p1), dnf_i(p2))
   | _ -> p;;
 
-let rec cnf (p: prop): prop = let pm = cnf_i(nnf(p)) in
-  match pm with 
-  | Or(p1, p2) -> raise CantConvert
-  | _ -> pm;;
+let rec cnf (p: prop): prop = cnf_i(nnf(p));;
 
-let rec dnf (p: prop): prop = let pm = dnf_i(nnf(p)) in
-  match pm with 
-  | And(p1, p2) -> raise CantConvert
-  | _ -> pm;;
+let rec dnf (p: prop): prop = dnf_i(nnf(p));;
+
+(* ****************************************************************************************** *)
+(*                                           Examples                                         *)
+(* ****************************************************************************************** *)
+
+(* ******************************************************** *)
+(*                      ht examples                         *)
+(* ******************************************************** *)
+
+let t1 = And(T, F);;
+let t2 = Or(T, F);;
+let t3 = Impl(t1, t2);;
+let t4 = Iff(t3, t1);;
+let t5 = Iff(Or(t2, t1), And(T, Impl(L("A"), F)));;
+
+ht t1;;
+ht t2;;
+ht t3;;
+ht t4;;
+
+ht t5;;
+
+(* ******************************************************** *)
+(*                     size Examples                        *)
+(* ******************************************************** *)
+
+size t1;;
+size t2;;
+size t3;;
+size t4;;
+
+size t5;;
+
+(* ******************************************************** *)
+(*                  letters Examples                        *)
+(* ******************************************************** *)
+
+let a = Or(L("a"), L("w"));;
+let b = And(L("b"), L("x"));;
+let c = Impl(L("c"), L("y"));;
+let d = Iff(L("d"), L("z"));;
+
+let t6 = Iff(Impl(a, b), Impl(c, Or(L("a"), And(c, d))));;
+let l1 = letters (t6);;
+
+(* ******************************************************** *)
+(*                   subprop Examples                       *)
+(* ******************************************************** *)
+
+let sp1 = subprop_at (Or(And(And(T,T),T), And(And(T,T),T))) (And(T, T));;
+let sp2 = subprop_at t6 c;;
+
+(* ******************************************************** *)
+(*                   truth Examples                         *)
+(* ******************************************************** *)
+
+let h1 = Hashtbl.create 100;;
+let h2 = Hashtbl.create 100;;
+let h3 = Hashtbl.create 100;;
+let h4 = Hashtbl.create 100;;
+
+Hashtbl.add h1 "a" true;;
+Hashtbl.add h1 "b" true;;
+Hashtbl.add h2 "a" true;;
+Hashtbl.add h2 "b" false;;
+Hashtbl.add h3 "a" false;;
+Hashtbl.add h3 "b" true;;
+Hashtbl.add h4 "a" false;;
+Hashtbl.add h4 "b" false;;
+
+let tauto1 = Iff(Impl(L("a"), L("b")), Impl(Not(L("b")), Not(L("a"))));;
+let truth1 = truth tauto1 (Tbl(h1));;
+let truth1 = truth tauto1 (Tbl(h2));;
+let truth1 = truth tauto1 (Tbl(h3));;
+let truth1 = truth tauto1 (Tbl(h4));;
+
+let tauto2 = Or(L("a"), Not(L("a")));;
+let truth2 = truth tauto2 (Tbl(h1));;
+let truth2 = truth tauto2 (Tbl(h2));;
+let truth2 = truth tauto2 (Tbl(h3));;
+let truth2 = truth tauto2 (Tbl(h4));;
+
+let contra1 = Iff(And(L("a"), L("b")), Or(Not(L("a")), Not(L("b"))));;
+let truth3 = truth contra1 (Tbl(h1));;
+let truth3 = truth contra1 (Tbl(h2));;
+let truth3 = truth contra1 (Tbl(h3));;
+let truth3 = truth contra1 (Tbl(h4));;
+
+let contra2 = And(L("b"), Not(L("b")));;
+let truth4 = truth contra2 (Tbl(h1));;
+let truth4 = truth contra2 (Tbl(h2));;
+let truth4 = truth contra2 (Tbl(h3));;
+let truth4 = truth contra2 (Tbl(h4));;
+
+let conti1 = Impl(L("a"), Not(Impl(L("b"), L("a"))));;
+let truth5 = truth conti1 (Tbl(h1));;
+let truth5 = truth conti1 (Tbl(h2));;
+let truth5 = truth conti1 (Tbl(h3));;
+let truth5 = truth conti1 (Tbl(h4));;
+
+let conti2 = Or(L("a"), L("b"));;
+let truth6 = truth conti2 (Tbl(h1));;
+let truth6 = truth conti2 (Tbl(h2));;
+let truth6 = truth conti2 (Tbl(h3));;
+let truth6 = truth conti2 (Tbl(h4));;
+
+(* ******************************************************** *)
+(*                    cnf, dnf and nnf                      *)
+(* ******************************************************** *)
+
+let p1 = And(And(And(And(Or(T, F), F), T), F), T);;
+let d1 = dnf p1;;
+
+let p2 = Iff(And(T,F), And(F, F));;
+let d2 = dnf p2;;
+
+let p3 = Or(And(T, F), Or(T, F));;
+let c1 = cnf p2;;
+
+let p4 = Impl(Iff(T, F), Not(T));;
+let c2 = cnf p2;;
+
+let p5 = Not(Not(Or(And(T, Not(F)), Not(And(L("a"), F)))));;
+let n1 = nnf p5;;
+
+(* ************************************************************************** *)
